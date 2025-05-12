3d27a03010adc3499eeaf68134a3efdc7910609b
jefrayn <salinasj.jr@gmail.com>
2025-05-12 08:51:14 -0400

    Initial Commit: setup with Next.js and Clerk

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+   <div>
+      <SignedOut>
+              <SignInButton />
+              {/* <SignUpButton /> */}
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+   </div>
+  );
+}
