662d44a46e8e0ba5458a5fa014749210ecb87e7e
jefrayn <salinasj.jr@gmail.com>
2025-05-17 14:24:40 -0400

    Authenticate Clerk with the database using Prism

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,4 @@
-
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
-
-import dynamic from "next/dynamic";
-
-const ModeToggle = dynamic(() => import("@/components/ModeToggle"), { ssr: false });
-
-
-export default function Home() {
+export default async function Home() {
   return (
    <div className="m-4">
       <h1>home page content</h1>
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,27 @@
-import { PrismaClient } from '@prisma/client'
+// import { PrismaClient } from '@prisma/client'
 
-const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
+// const prismaClientSingleton = () => {
+//   return new PrismaClient()
+// }
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>
-} & typeof global
+// declare const globalThis: {
+//   prismaGlobal: ReturnType<typeof prismaClientSingleton>
+// } & typeof global
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+// const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
 
-export default prisma
+// export default prisma;
 
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+// if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+
+
+// lib/prisma.ts
+import { PrismaClient } from '@prisma/client';
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined };
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
+export default prisma;
