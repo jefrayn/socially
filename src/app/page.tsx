9b86cd53d8bf140b480c456c7d568ce718094e6a
jefrayn <salinasj.jr@gmail.com>
2025-05-17 09:26:57 -0400

    Button Modification Made

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,10 @@ export default function Home() {
   return (
    <div>
       <SignedOut>
-              <SignInButton mode ="modal"/>             
+              <SignInButton mode ="modal">               
+                  <button className="bg-red-500">Sign in</button>  
+              </SignInButton>   
+                             
             </SignedOut>
             <SignedIn>
               <UserButton />
