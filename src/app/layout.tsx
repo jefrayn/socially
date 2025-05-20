9dc84eba89d042db6154db3411563e500700ed4f
jefrayn <salinasj.jr@gmail.com>
2025-05-20 09:53:30 -0400

    All the remaining modifications made, including profile build, follow and like button builds, adding/deleting posts, notification implementation, uploadthing implementation, uploading images, etc.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/ui/ThemeProvider";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -61,6 +62,7 @@ export default function RootLayout({
             
            </main>
             </div>
+            <Toaster />
         </ThemeProvider>
       </body>
     </html>
